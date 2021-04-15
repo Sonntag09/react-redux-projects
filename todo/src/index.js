3f393d3a91eb4de5ff745a57ec6a498256a9cdb4
Sonntag09 <sonntagcode@gmail.com>
2021-04-15 14:12:15 +0300

    todo v0.1

---
+++ todo/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const el = (
+  <div>
+    <h1>My Todo List</h1>
+    <input placeholder='search' />
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(el,
+   document.getElementById('root'));
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
