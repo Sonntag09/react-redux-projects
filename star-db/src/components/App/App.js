fd45ce063cbe60db27a4a13b11404231044b9449
Sonntag09 <sonntagcode@gmail.com>
2021-04-30 13:42:19 +0300

    star-db v0.2

---
+++ star-db/src/components/App/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+import Header from '../Header';
+import RandomPlanet from '../RandomPlanet';
+import ItemList from '../ItemList';
+import PersonDetails from '../PersonDetails';
+
+import './App.css';
+
+export default class App extends Component {
+
+  state = {
+    showRandomPlanet: true
+  };
+
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
+    });
+  };
+
+  render() {
+
+    const planet = this.state.showRandomPlanet ?
+      <RandomPlanet/> :
+      null;
+
+    return (
+      <div className="stardb-app">
+        <Header />
+        { planet }
+
+        <button
+          className="toggle-planet btn btn-warning btn-lg"
+          onClick={this.toggleRandomPlanet}>
+          Toggle Random Planet
+        </button>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,60 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-class SwapiService {
-
-  _apiBase = 'https://swapi.dev/api';
-
-  async getResource(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
-
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${this._apiBase}${url}, received ${res.status}`);
-    }
-
-    return await res.json();
-  }
-
-  async getAllPeople() {
-    const res = await this.getResource(`/people/`);
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResource(`/people/${id}/`);
-  }
-
-  async getAllPlanets() {
-    const res = await this.getResource(`/planets/`);
-    return res.results;
-  }
-
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}/`);
-  }
-
-  async getAllStarships() {
-    const res = await this.getResource(`/starships/`);
-    return res.results;
-  }
-
-  getStarship(id) {
-    return this.getResource(`/starships/${id}/`);
-  }
-}
-
-const swapi = new SwapiService();
-swapi.getAllPeople().then((people) => {
-  people.forEach((p) => {
-    console.log(p.name);
-  });  
-});
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
